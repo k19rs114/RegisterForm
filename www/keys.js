99119c027867c63d267c78747eb78de614106a5a
k19rs114 <k19rs114@st.kyusan-u.ac.jp>
2021-01-05 11:11:32 +0900

    Add appKey and cilentKey

---
+++ www/keys.js
@@ -0,0 +1,5 @@
+// This is a JavaScript file
+var appKey="ee06057d28dd501bbde2a1687c9df99208f8647ca063f057db278795e52a94c7";
+var clientKey="97e9afe24ffb136408644516b20e437274e38700f1253b2e67375ab0c9b0beb5";
+
+
